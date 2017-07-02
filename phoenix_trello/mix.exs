8a3ae5a37cf933cf9c13df8789389f747568a5fd
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-02 23:41:59 +0200

    encryped password

--- phoenix_trello/mix.exs
+++ phoenix_trello/mix.exs
@@ -19,7 +19,7 @@ defmodule PhoenixTrello.Mixfile do
   def application do
     [mod: {PhoenixTrello, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
