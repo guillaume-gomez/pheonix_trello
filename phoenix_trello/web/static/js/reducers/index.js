c38ad5aafec7dffbf217933488b2fc23741468e3
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-02 22:22:39 +0200

    add router reducer

--- phoenix_trello/web/static/js/reducers/index.js
+++ phoenix_trello/web/static/js/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import session from './session'
 import counter from './counter'
+import { routeReducer } from 'redux-simple-router';
 
 export default combineReducers({
   session,
-  counter
+  counter,
+  routing: routeReducer,
 });
 //export default counter;
