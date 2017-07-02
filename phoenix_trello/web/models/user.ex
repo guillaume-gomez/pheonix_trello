ddb18190b8dedfe3237a296dcd14b6423d19e3d8
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-03 00:03:34 +0200

    update user model

--- phoenix_trello/web/models/user.ex
+++ phoenix_trello/web/models/user.ex
@@ -1,22 +1,34 @@
 defmodule PhoenixTrello.User do
   use PhoenixTrello.Web, :model
 
+  alias PhoenixTrello.{Board, UserBoard}
+
+  @derive {Poison.Encoder, only: [:id, :first_name, :last_name, :email]}
+
   schema "users" do
     field :first_name, :string
     field :last_name, :string
     field :email, :string
     field :encrypted_password, :string
+    field :password, :string, virtual: true
+
+    has_many :owned_boards, Board
+    has_many :user_boards, UserBoard
+    has_many :boards, through: [:user_boards, :board]
 
     timestamps()
   end
 
-  @required_fields ~w(first_name last_name email)
+  @required_fields ~w(first_name last_name email password)
   @optional_fields ~w(encrypted_password)
+
   @doc """
-  Builds a changeset based on the `struct` and `params`.
+  Creates a changeset based on the `model` and `params`.
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
+  def changeset(model, params \\ %{}) do
+    model
     |> cast(params, @required_fields, @optional_fields)
     |> validate_format(:email, ~r/@/)
     |> validate_length(:password, min: 5)
