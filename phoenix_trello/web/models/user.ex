91726d5c7764c6baad84be4c5a5b14a26311edae
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-02 23:26:37 +0200

    create user model

---
+++ phoenix_trello/web/models/user.ex
@@ -0,0 +1,26 @@
+defmodule PhoenixTrello.User do
+  use PhoenixTrello.Web, :model
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :encrypted_password, :string
+
+    timestamps()
+  end
+
+  @required_fields ~w(first_name last_name email)
+  @optional_fields ~w(encrypted_password)
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @required_fields, @optional_fields)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 5)
+    |> validate_confirmation(:password, message: "Password does not match")
+    |> unique_constraint(:email, message: "Email already taken")
+  end
+end
