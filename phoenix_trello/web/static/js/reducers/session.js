10a823e8b7e7386080d6a9e89b0e5fcf2cdde2d0
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-11 09:57:18 +0200

    update session reducer

--- phoenix_trello/web/static/js/reducers/session.js
+++ phoenix_trello/web/static/js/reducers/session.js
@@ -2,12 +2,17 @@ import Constants from '../constants';
 
 const initialState = {
   currentUser: null,
+  error: null,
 };
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case Constants.CURRENT_USER:
       return Object.assign({}, state, currentUser: action.currentUser);
+    case Constants.SESSIONS_ERROR:
+      return Object.assign({}, state, { error: action.error });
+    case Constants.USER_SIGNED_OUT:
+      return initialState;
     default:
       return state;
   }
