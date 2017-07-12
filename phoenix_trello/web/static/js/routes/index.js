04ce51971058055f75112d908087f07f37331240
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-07-12 19:20:04 +0200

    add current user model + controller

--- phoenix_trello/web/static/js/routes/index.js
+++ phoenix_trello/web/static/js/routes/index.js
@@ -2,6 +2,8 @@ import { IndexRoute, Route } from 'react-router';
 import React from 'react';
 import MainLayout from "../layouts/MainLayout.react";
 import Counter from '../components/Counter.react';
+import AuthenticatedContainer from "../containers/authenticatedContainer.react";
+import HomeIndexView from '../views/home';
 import RegistrationsNew from "../views/registrations/new";
 import SessionsNew from '../views/sessions/new';
 
@@ -10,6 +12,8 @@ export default (
     <Route path="/sign_up" component={RegistrationsNew} />
     <Route path="/sign_in" component={SessionsNew} />
 
-    <Route path="/" component={Counter} />
+    <Route path="/" component={AuthenticatedContainer}>
+      <IndexRoute component={HomeIndexView} />
+    </Route>
   </Route>
 );
